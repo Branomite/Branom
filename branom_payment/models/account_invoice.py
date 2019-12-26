4c88bdb2a2077d617a99fe363ab6f58643fd48c3
ehe <ehe@odoo.com>
2019-12-26 19:39:32 +0000

    [ADD][2153456]

---
+++ branom_payment/models/account_invoice.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    payment_difference = fields.Monetary(string='Payment Difference', help="Payment difference in invoice's currency.")
+
