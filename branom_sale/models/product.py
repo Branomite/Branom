3711e92b1fc7f1b33edc3c481c15e04da8cdff9d
cic-odoo <cic@odoo.com>
2020-02-18 17:00:15 -0800

    [IMP] Auto create vendor pricelist

--- branom_sale/models/product.py
+++ branom_sale/models/product.py
@@ -8,15 +8,15 @@ from odoo.tools.float_utils import float_compare, float_round
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
-
+    # TODO: remove this field properly.
     cost_extra = fields.Float(
         string='Cost Extra',
-        company_dependent=True,
+        # company_dependent=True,
         digits=dp.get_precision('Product Price'),
-        compute='_compute_product_cost_extra',
-        readonly=True,
-        groups='base.group_user',
-        store=True,
+        # compute='_compute_product_cost_extra',
+        # readonly=True,
+        # groups='base.group_user',
+        # store=True,
     )
 
     @api.depends('product_template_attribute_value_ids', 'product_template_attribute_value_ids.cost_extra')
@@ -40,7 +40,6 @@ class ProductProduct(models.Model):
         code = self.product_tmpl_id.base_default_code or ''
         prefix, suffix = '', ''
 
-
         for attr_val in self.attribute_value_ids.sorted(key=lambda r: r.position):
             if attr_val.affix_type == 'prefix':
                 prefix += attr_val.manufacture_code + attr_val.separator
@@ -51,8 +50,24 @@ class ProductProduct(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         res = super(ProductProduct, self).create(vals_list)
-        for prod in res.filtered(lambda p: p.attribute_value_ids):
-            prod.default_code = prod.generate_extra_code()
+        for prod in res:
+            # Create vendor list for each newly created product
+            vendor_ids = prod.product_tmpl_id.variant_seller_ids.mapped('name')
+
+            if len(vendor_ids) == 1:
+                vendor_list = self.env['product.supplierinfo'].create([{
+                    'name': vendor_ids[0].id,
+                    'product_tmpl_id': prod.product_tmpl_id.id,
+                    'product_id': prod.id,
+                }])
+                print(vendor_list)
+            else:
+                # TODO: add warning popup that pricelist won't be created with 2+ vendors
+                print("Placeholder for 0 or 2+ vendors warning")
+
+            # only create code if has attr_values
+            if prod.attribute_value_ids:
+                prod.default_code = prod.generate_extra_code()
         return res
 
 
@@ -66,6 +81,14 @@ class ProductTemplate(models.Model):
 
     base_default_code = fields.Char(string='Base Internal Reference')
 
+    @api.multi
+    def write(self, vals):
+        res = super(ProductTemplate, self).write(vals)
+        for tmpl in self.filtered(lambda t: len(t.product_variant_ids) == 1):
+            prod = tmpl.product_variant_id
+            prod.default_code = prod.generate_extra_code()
+        return res
+
 
 class SupplierInfo(models.Model):
     _inherit = 'product.supplierinfo'
@@ -78,6 +101,7 @@ class SupplierInfo(models.Model):
         digits=dp.get_precision('Product Price'),
         default=0.0,
         help='The extra cost of this variant based on attribute value\'s cost_extra.',
+        store=True,
     )
 
     price_with_extra = fields.Float(
@@ -89,32 +113,18 @@ class SupplierInfo(models.Model):
         store=True,
     )
 
-    # TODO: remove the other dependencies, might be too slow, need to convert to action talk to ALB
+    # Compute the cost extra: sum upp the cost_extra for each attribute value on product.product
     # @api.depends('product_id', 'product_id.product_template_attribute_value_ids', 'product_id.product_template_attribute_value_ids.cost_extra')
-    @api.depends('product_id')
+    @api.depends('product_id', 'product_id.attribute_value_ids')
     def _compute_product_cost_extra(self):
-        for vendor_list in self.filtered(lambda v: v.product_id and v.product_id.product_template_attribute_value_ids):
-            for value in vendor_list.product_id.product_template_attribute_value_ids:
+        for vendor_list in self.filtered(lambda v: v.product_id and v.product_id.attribute_value_ids):
+            for value in vendor_list.product_id.attribute_value_ids:
                 vendor_list.cost_extra += value.cost_extra
 
-    @api.depends('product_id', 'product_id.standard_price', 'cost_extra')
+    # Compute the total extra cost, includes: product.template's base_standard_price + product.supplierinfo's cost_extra
+    @api.depends('product_tmpl_id', 'product_tmpl_id.base_standard_price', 'cost_extra')
     def _compute_cost_variant(self):
-        for vendor_list in self.filtered(lambda x: x.product_id):
-            vendor_list.price_with_extra = vendor_list.product_id.standard_price + vendor_list.cost_extra
-            # if cost:
-            #     self.price_with_extra = cost
-            # else:
-            #     for rec in self:
-            #         rec.price = rec.price_variant
-
-            # set Odoo price field to the new calculated field that has extra cost + list price
+        for vendor_list in self.filtered(lambda x: x.product_tmpl_id):
+            vendor_list.price_with_extra = vendor_list.product_tmpl_id.base_standard_price + vendor_list.cost_extra
+            # Automatically set the price on vendor pricelist
             vendor_list.price = vendor_list.price_with_extra
-        # TODO: check what to pass into this func below
-    #     self._set_cost_variant(1)
-    #
-    # def _set_cost_variant(self, cost):
-    #     if cost:
-    #         self.price_variant = cost
-    #     else:
-    #         for rec in self:
-    #             rec.price = rec.price_variant
--- branom_sale/models/product_attribute.py
+++ branom_sale/models/product_attribute.py
@@ -22,31 +22,7 @@ class ProductAttributeValue(models.Model):
                                   selection=[('prefix', 'Prefix'),
                                              ('suffix', 'Suffix')])
 
-
-class ProductTemplateAttributeLine(models.Model):
-    _inherit = 'product.template.attribute.line'
-
-    # TODO: check with alb for best course of action. We might need a server action. and to handle
-    # this in a computed field for the computation of the code
-    @api.model_create_multi
-    def create(self, vals):
-        res = super(ProductTemplateAttributeLine, self).create(vals)
-        print("i'm inside create")
-
-        # variant_alone = self.product_tmpl_id._get_valid_product_template_attribute_lines().filtered(
-        #     lambda line: line.attribute_id.create_variant == 'always' and len(line.value_ids) == 1).mapped('value_ids')
-
-        # check all lines in the original product template, if they all have only 1, then we need to update the
-        # single product's internal reference
-        for attr_line in res:
-            all_is_one = all(len(line.value_ids) == 1 for line in attr_line.product_tmpl_id.valid_product_template_attribute_line_ids)
-            if len(attr_line.product_tmpl_id.product_variant_ids) == 1:
-                prod = attr_line.product_tmpl_id.product_variant_id
-                prod.default_code = prod.generate_extra_code()
-        return res
-
-    @api.multi
-    def write(self, vals):
-        res = super(ProductTemplateAttributeLine, self).write(vals)
-        print("i'm inside the write")
-        return res
+    cost_extra = fields.Float(string='Attribute Extra Cost', default=0.0,
+                              digits=dp.get_precision('Product Price'),
+                              help='Extra Cost: Extra cost for the variant with this attribute value on sale price. '
+                                   'eg. 200 cost extra, 1000 + 200 = 1200.')
