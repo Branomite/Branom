845fa9de8ab39fd3677e16657a35fb03abb11a18
Kunal Chaudhary Rajora <kunal.chaudhary961@webkul.com>
2020-11-18 14:41:33 +0530

    website_also_bought_products added

---
+++ website_also_bought_products/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+# Copyright (c) 2018-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>:wink:
+# See LICENSE file for full copyright and licensing details.
+#################################################################################
+from . import product
+from . import sale
+from . import res_config
+from . import also_bought_website_products
---
+++ website_also_bought_products/models/also_bought_website_products.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+# Copyright (c) 2018-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>:wink:
+# See LICENSE file for full copyright and licensing details.
+#################################################################################
+
+
+from odoo import api, models, fields
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class Website(models.Model):
+	_inherit = 'website'
+
+	use_automatic = fields.Boolean(
+		string="Add Products Automatically",
+		help="Add the products automatically to the also bought on the purchase"
+	)
+
+	abtp_header = fields.Char(
+		string="Header Message",
+		required=True,
+	)
