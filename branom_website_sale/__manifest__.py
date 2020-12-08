3016205e4bbd0f5ab2ceb93a7974c8efff2a71cc
Cedric Collins <cedric@hibou.io>
2020-12-08 15:58:38 -0600

    [ADD] branom_website_sale: quick order form
    H4564

---
+++ branom_website_sale/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Branom Website Sale',
+    'author': 'Hibou Corp. <hello@hibou.io>',
+    'version': '13.0.1.0.0',
+    'category': 'Sale',
+    'description': """
+Customizations to website_sale
+    """,
+    'website': 'https://hibou.io/',
+    'depends': [
+        'crm',
+        'sale',
+        'website_sale',
+    ],
+    'data': [
+        'views/quick_order_templates.xml',
+        'views/web_assets.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
