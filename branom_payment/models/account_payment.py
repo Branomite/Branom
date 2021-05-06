dc75480416df32e33a081ee5e543582304981a5a
Cedric Collins <cedric@hibou.io>
2021-05-06 11:13:48 -0500

    [IMP] branom_payments: resolve incorrect calculations
    H5612

--- branom_payment/models/account_payment.py
+++ branom_payment/models/account_payment.py
@@ -18,63 +18,6 @@ MAP_INVOICE_TYPE_PAYMENT_SIGN = {
     'out_refund': 1,
 }
 
-# deprecated
-# class AccountAbstractPayment(models.AbstractModel):
-#     _inherit = 'account.abstract.payment'
-
-#     @api.multi
-#     def _compute_payment_amount(self, invoices=None, currency=None, with_discount=True):
-#         if not with_discount:
-#             return super(AccountAbstractPayment, self)._compute_payment_amount(invoices=invoices, currency=currency)
-
-#         # Get the payment invoices
-#         if not invoices:
-#             invoices = self.invoice_ids
-
-#         # Get the payment currency
-#         payment_currency = currency
-#         if not payment_currency:
-#             payment_currency = self.currency_id or self.journal_id.currency_id or self.journal_id.company_id.currency_id or invoices and invoices[0].currency_id
-
-#         # Avoid currency rounding issues by summing the amounts according to the company_currency_id before
-#         invoice_datas = invoices.read_group(
-#             [('id', 'in', invoices.ids)],
-#             ['currency_id', 'type', 'residual_signed', 'residual_company_signed', 'amount_total_signed', 'amount_total_company_signed', 'payment_term_id', 'amount_discounted_signed', 'amount_discounted_company_signed', 'amount_undiscounted_signed', 'amount_undiscounted_company_signed'],
-#             ['currency_id', 'type', 'payment_term_id'], lazy=False)
-#         total = 0.0
-#         for invoice_data in invoice_datas:
-#             sign = MAP_INVOICE_TYPE_PAYMENT_SIGN[invoice_data['type']]
-#             discount = 0.0
-#             if invoice_data['payment_term_id']:
-#                 payment_term = self.env['account.payment.term'].browse(invoice_data['payment_term_id'][0])
-#                 discount = payment_term.discount
-
-#             paid_total = sign * (invoice_data['amount_total_signed'] - invoice_data['residual_signed'])
-#             paid_total_company = sign * (invoice_data['amount_total_company_signed'] - invoice_data['residual_company_signed'])
-#             # if discount is 0.0, do we risk the float precision here?
-#             amount_total = sign * invoice_data['amount_discounted_signed'] * (1.0 - discount) + sign * invoice_data['amount_undiscounted_signed'] - paid_total
-#             amount_total_company_signed = sign * invoice_data['amount_discounted_company_signed'] * (1.0 - discount) + sign * invoice_data['amount_undiscounted_company_signed'] - paid_total_company
-#             invoice_currency = self.env['res.currency'].browse(invoice_data['currency_id'][0])
-#             if payment_currency == invoice_currency:
-#                 total += amount_total
-#             else:
-#                 # Here there is no chance we will reconcile on amount_currency
-#                 # Hence, we need to compute with the amount in company currency as the base
-#                 total += self.journal_id.company_id.currency_id._convert(
-#                     amount_total_company_signed,
-#                     payment_currency,
-#                     self.env.user.company_id,
-#                     self.payment_date or fields.Date.today()
-#                 )
-#         return total
-        
-#     @api.depends('invoice_ids', 'amount', 'payment_date', 'currency_id')
-#     def _compute_payment_difference(self):
-#         super(AccountAbstractPayment, self)._compute_payment_difference()
-#         for pay in self.filtered(lambda p: p.invoice_ids):
-#             payment_amount = -pay.amount if pay.payment_type == 'outbound' else pay.amount
-#             pay.payment_difference = pay._compute_payment_amount(with_discount=False) - payment_amount
-
 
 class AccountPaymentTerm(models.Model):
     _inherit = "account.payment.term"
@@ -104,7 +47,7 @@ class AccountPayment(models.Model):
     def post(self):
         res = super(AccountPayment, self).post()
         if res and self.payment_difference_handling == 'reconcile' and self.payment_difference:
-            total = sum(self.invoice_ids.mapped('amount_total'))
+            total = sum(self.invoice_ids.mapped('amount_discounted_signed'))
             for invoice in self.invoice_ids:
                 invoice.write({
                     'payment_difference': self.payment_difference * (invoice.amount_discounted_signed / total)
@@ -132,48 +75,29 @@ class AccountPayment(models.Model):
         if not invoices:
             return 0.0
 
-        self.env['account.move'].flush(['type', 'currency_id'])
-        self.env['account.move.line'].flush(['amount_residual', 'amount_residual_currency', 'move_id', 'account_id'])
-        self.env['account.account'].flush(['user_type_id'])
-        self.env['account.account.type'].flush(['type'])
-        self._cr.execute('''
-                SELECT
-                    move.type AS type,
-                    move.currency_id AS currency_id,
-                    move.invoice_payment_term_id AS payment_term_id,
-                    move.amount_total_signed AS amount_total_company_signed,
-                    move.amount_discounted_signed AS amount_discounted_signed,
-                    move.amount_undiscounted_signed AS amount_undiscounted_signed,
-                    move.amount_discounted_company_signed AS amount_discounted_company_signed,
-                    move.amount_undiscounted_company_signed AS amount_undiscounted_company_signed,
-                    SUM(line.price_total) AS amount_total_signed,
-                    SUM(line.amount_residual) AS amount_residual,
-                    SUM(line.amount_residual_currency) AS residual_currency
-                FROM account_move move
-                LEFT JOIN account_move_line line ON line.move_id = move.id
-                LEFT JOIN account_account account ON account.id = line.account_id
-                LEFT JOIN account_account_type account_type ON account_type.id = account.user_type_id
-                WHERE move.id IN %s
-                AND account_type.type IN ('receivable', 'payable')
-                GROUP BY move.id, move.type
-            ''', [tuple(invoices.ids)])
-        query_res = self._cr.dictfetchall()
+        # Avoid currency rounding issues by summing the amounts according to the company_currency_id before
+        invoice_datas = invoices.read_group(
+            [('id', 'in', invoices.ids)],
+            ['currency_id', 'type', 'amount_residual', 'amount_residual_signed', 'amount_total', 'amount_total_signed', 'invoice_payment_term_id', 'amount_discounted_signed', 'amount_discounted_company_signed', 'amount_undiscounted_signed', 'amount_undiscounted_company_signed'],
+            ['currency_id', 'type', 'invoice_payment_term_id'], lazy=False)
 
         total = 0.0
-        for res in query_res:
+        for res in invoice_datas:
             discount = 0.0
-            if res['payment_term_id']:
-                payment_term = self.env['account.payment.term'].browse(res['payment_term_id'])
+            if res['invoice_payment_term_id']:
+                payment_term = self.env['account.payment.term'].browse(res['invoice_payment_term_id'][0])
                 # TODO: get discount from term lines based on date and remove manually-entered discount value
                 discount = payment_term.discount
 
             sign = MAP_INVOICE_TYPE_PAYMENT_SIGN[res['type']]
-            paid_total = sign * (res['amount_total_signed'] - res['residual_currency'])
-            paid_total_company = sign * (res['amount_total_company_signed'] - res['amount_residual'])
+            # Amounts in the invoice currency are not signed
+            paid_total = sign * res['amount_total'] - sign * res['amount_residual']
+            # Amounts in company currency are already signed
+            paid_total_company = company.currency_id.round(res['amount_total_signed'] - res['amount_residual_signed'])
             # if discount is 0.0, do we risk the float precision here?
             amount_total = sign * res['amount_discounted_signed'] * (1.0 - discount) + sign * res['amount_undiscounted_signed'] - paid_total
-            amount_total_company_signed = sign * res['amount_discounted_company_signed'] * (1.0 - discount) + sign * res['amount_undiscounted_company_signed'] - paid_total_company
-            move_currency = self.env['res.currency'].browse(res['currency_id'])
+            amount_total_company_signed = company.currency_id.round(sign * res['amount_discounted_company_signed'] * (1.0 - discount) + sign * res['amount_undiscounted_company_signed'] - paid_total_company)
+            move_currency = self.env['res.currency'].browse(res['currency_id'][0])
             if move_currency == currency and move_currency != company.currency_id:
                 total += amount_total
             else:
